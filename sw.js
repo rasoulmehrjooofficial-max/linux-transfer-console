5ba2efa0d100f3e1a7e2467aaf7ef5e051448315
rama <rama@users.noreply.github.com>
2026-09-18 14:18:47 -0700

    Use opaque black status bar instead of black-translucent to fix notch overlap/dimming

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    to the network normally.
    ============================================================================ */
 
-const CACHE_NAME = "rama-console-v2";
+const CACHE_NAME = "rama-console-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
